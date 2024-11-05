973108aa297169cc3a4d91b7a602e53a42d6e52a
Jalil <zendyani@gmail.com>
2024-11-05 21:30:53 +0100

    Added PHPStan and Added explicit type for PlayerRepository and Response

--- src/UseCase/GetPlayers/Response.php
+++ src/UseCase/GetPlayers/Response.php
@@ -6,6 +6,9 @@ use App\Domain\Entity\Player;
 
 final class Response
 {
+    /**
+     * @param array<Player> $players
+     */
     public function __construct(private readonly array $players)
     {
     }
